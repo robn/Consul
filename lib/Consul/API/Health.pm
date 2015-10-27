0e4840483be2445d1eed7b6748045ae94e2286d4
Robert Norris <rob@eatenbyagrue.org>
2015-10-27 21:31:44 +1100

    Implement health/service

--- lib/Consul/API/Health.pm
+++ lib/Consul/API/Health.pm
@@ -38,8 +38,9 @@ sub checks {
 }
 
 sub service {
-    # service
-    croak "not yet implemented";
+    my ($self, $service, %args) = @_;
+    croak 'usage: $health->service($service, [%args])' if grep { !defined } ($service);
+    map { Consul::API::Health::Service->new(%$_) } @{decode_json($$self->api_exec($$self->_health_endpoint."/service/".$service, 'GET', %args)->{content})};
 }
 
 sub state {
@@ -61,5 +62,15 @@ has notes        => ( is => 'ro', isa => Str, init_arg => 'Notes',       require
 has output       => ( is => 'ro', isa => Str, init_arg => 'Output',      required => 1 );
 has service_id   => ( is => 'ro', isa => Str, init_arg => 'ServiceID',   required => 1 );
 has service_name => ( is => 'ro', isa => Str, init_arg => 'ServiceName', required => 1 );
+
+package Consul::API::Health::Service;
+
+use Moo;
+use Types::Standard qw(ArrayRef);
+use Type::Utils qw(class_type);
+
+has node    => ( is => 'ro', isa => class_type('Consul::API::Catalog::ShortNode'),      init_arg => 'Node',    required => 1, coerce => sub { Consul::API::Catalog::ShortNode->new($_[0]) } );
+has service => ( is => 'ro', isa => class_type('Consul::API::Agent::Service'),          init_arg => 'Service', required => 1, coerce => sub { Consul::API::Agent::Service->new($_[0]) } );
+has checks  => ( is => 'ro', isa => ArrayRef[class_type('Consul::API::Health::Check')], init_arg => 'Checks',  required => 1, coerce => sub { [ map { Consul::API::Health::Check->new($_) } @{$_[0]} ] } );
 
 1;
