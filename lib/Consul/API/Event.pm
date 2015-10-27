1f02937d7215fdf8001b2677111cd3ead1ef3105
Robert Norris <rob@eatenbyagrue.org>
2015-10-27 23:58:29 +1100

    Implemented Event API

--- lib/Consul/API/Event.pm
+++ lib/Consul/API/Event.pm
@@ -26,13 +26,31 @@ use JSON::MaybeXS;
 use Carp qw(croak);
 
 sub fire {
-    # fire
-    croak "not yet implemented";
+    my ($self, $name, %args) = @_;
+    croak 'usage: $event->fire($name, [%args])' if grep { !defined } ($name);
+    my $payload = delete $args{payload};
+    Consul::API::Event::Event->new(decode_json($$self->api_exec($$self->_event_endpoint."/fire/".$name, 'PUT', %args, ($payload ? (_content => $payload) : ()))->{content}));
 }
 
 sub list {
-    # list
-    croak "not yet implemented";
+    my ($self, %args) = @_;
+    map { Consul::API::Event::Event->new(%$_) } @{decode_json($$self->api_exec($$self->_event_endpoint."/list", 'GET', %args)->{content})};
 }
+
+package Consul::API::Event::Event;
+
+use Convert::Base64 qw(decode_base64);
+
+use Moo;
+use Types::Standard qw(Str Int Maybe);
+
+has id             => ( is => 'ro', isa => Str,        init_arg => 'ID',            required => 1 );
+has name           => ( is => 'ro', isa => Str,        init_arg => 'Name',          required => 1 );
+has payload        => ( is => 'ro', isa => Maybe[Str], init_arg => 'Payload',       required => 1, coerce => sub { defined $_[0] ? decode_base64($_[0]) : undef});
+has node_filter    => ( is => 'ro', isa => Str,        init_arg => 'NodeFilter',    required => 1 );
+has service_filter => ( is => 'ro', isa => Str,        init_arg => 'ServiceFilter', required => 1 );
+has tag_filter     => ( is => 'ro', isa => Str,        init_arg => 'TagFilter',     required => 1 );
+has version        => ( is => 'ro', isa => Int,        init_arg => 'Version',       required => 1 );
+has l_time         => ( is => 'ro', isa => Int,        init_arg => 'LTime',         required => 1 );
 
 1;
